db247ebab2519eea2d2565cfcd4b657e7b29591c
RamaTineo <ramatineo@gmail.com>
2023-02-12 21:38:17 -0300

    agregado de firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpYUFCpXOhULFGNOswcv5Ofbge1P1RdVo",
+  authDomain: "el-negocio-de-atlas.firebaseapp.com",
+  projectId: "el-negocio-de-atlas",
+  storageBucket: "el-negocio-de-atlas.appspot.com",
+  messagingSenderId: "1075926696135",
+  appId: "1:1075926696135:web:2187c1f9f5b27ff054bee2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
